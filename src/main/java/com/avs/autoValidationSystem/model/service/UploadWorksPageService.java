1729db84298c26ace3cc308a97d99bb99cc543d3
Dmitry Vantsyn <davanc236@yandex.ru>
2023-03-21 21:37:00 +0300

    feat(upload works page): добавил эндпоинт для получения списка всех групп

--- src/main/java/com/avs/autoValidationSystem/model/service/UploadWorksPageService.java
+++
@@ -1,7 +0,0 @@
-package com.avs.autoValidationSystem.model.service;
-
-import com.avs.autoValidationSystem.model.dto.uploadlWorksPage.UploadWorkPageResultDto;
-
-public interface UploadWorksPageService {
-    UploadWorkPageResultDto getDataToUploadWorksPage();
-}
--- src/main/java/com/avs/autoValidationSystem/model/service/impl/UploadWorksPageServiceImpl.java
+++ src/main/java/com/avs/autoValidationSystem/model/service/impl/UploadWorksPageServiceImpl.java
@@ -1,16 +1,20 @@
 package com.avs.autoValidationSystem.model.service.impl;
 
-import com.avs.autoValidationSystem.model.dto.uploadlWorksPage.UploadWorkPageResultDto;
+import com.avs.autoValidationSystem.model.entity.Group;
 import com.avs.autoValidationSystem.model.repository.ControlWorkRepository;
 import com.avs.autoValidationSystem.model.repository.GroupRepository;
 import com.avs.autoValidationSystem.model.repository.OptionRepository;
 import com.avs.autoValidationSystem.model.repository.StudentRepository;
-import com.avs.autoValidationSystem.model.service.UploadWorksPageService;
+import com.avs.autoValidationSystem.model.service.UploadWorksService;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Service
-public class UploadWorksPageServiceImpl implements UploadWorksPageService {
+public class UploadWorksPageServiceImpl implements UploadWorksService {
     private final ControlWorkRepository controlWorkRepository;
     private final StudentRepository studentRepository;
     private final GroupRepository groupRepository;
@@ -23,15 +27,21 @@ public class UploadWorksPageServiceImpl implements UploadWorksPageService {
         this.optionRepository = optionRepository;
     }
 
+    //todo: закомментировано для дальнейшего использования, когда перестанет быть нужным, удалить
+//    @Override
+//    public UploadWorkPageResultDto getDataToUploadWorksPage() {
+//        UploadWorkPageResultDto uploadWorkPageResultDto = new UploadWorkPageResultDto();
+//
+//        uploadWorkPageResultDto.setControlWorks(controlWorkRepository.findAll());
+//        uploadWorkPageResultDto.setStudents(studentRepository.findAll(Sort.by("lastName")));
+//        uploadWorkPageResultDto.setGroups(groupRepository.findAll(Sort.by("name")));
+//        uploadWorkPageResultDto.setOptions(optionRepository.findAll());
+//
+//        return uploadWorkPageResultDto;
+//    }
+
     @Override
-    public UploadWorkPageResultDto getDataToUploadWorksPage() {
-        UploadWorkPageResultDto uploadWorkPageResultDto = new UploadWorkPageResultDto();
-
-        uploadWorkPageResultDto.setControlWorks(controlWorkRepository.findAll());
-        uploadWorkPageResultDto.setStudents(studentRepository.findAll(Sort.by("lastName")));
-        uploadWorkPageResultDto.setGroups(groupRepository.findAll(Sort.by("name")));
-        uploadWorkPageResultDto.setOptions(optionRepository.findAll());
-
-        return uploadWorkPageResultDto;
+    public Set<Group> getAllGroups() {
+        return new HashSet<>(groupRepository.findAll(Sort.by("name")));
     }
 }
