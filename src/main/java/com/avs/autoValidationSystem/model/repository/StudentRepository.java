6495aabd43f32d945c9794d1632f585479c0bcda
Dmitry Vantsyn <davanc236@yandex.ru>
2023-03-23 18:50:35 +0300

    feat(uploadWorksPage): мини очистка кода

--- src/main/java/com/avs/autoValidationSystem/model/repository/StudentRepository.java
+++ src/main/java/com/avs/autoValidationSystem/model/repository/StudentRepository.java
@@ -1,13 +1,7 @@
 package com.avs.autoValidationSystem.model.repository;
 
-import com.avs.autoValidationSystem.model.entity.ControlWork;
 import com.avs.autoValidationSystem.model.entity.Student;
-import com.avs.autoValidationSystem.model.entity.StudyGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
