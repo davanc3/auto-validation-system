4d0f38ab7b34983ad78fb1518c804cafda8c5f39
Efim <ziniakov.efim@gmail.com>
2023-03-23 18:22:47 +0300

    Изменил название таблицы option

--- src/main/java/com/avs/autoValidationSystem/model/repository/StudentRepository.java
+++ src/main/java/com/avs/autoValidationSystem/model/repository/StudentRepository.java
@@ -10,7 +10,5 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    @Query("SELECT student FROM Student as student join ControlWork as c on (c.id = student.controlWorks) " +
-            "WHERE (:studyGroup is null or student.studyGroup = :studyGroup)")
-    List<Student> findStudentsByFilter(@Param("studyGroup") StudyGroup studyGroup, @Param("controlWork") String controlWork);
+
 }
