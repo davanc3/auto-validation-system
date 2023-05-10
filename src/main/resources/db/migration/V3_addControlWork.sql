0f2cb0ac48c82dfa58bd343de5e48174e4ee2bb4
Dmitry Vantsyn <davanc236@yandex.ru>
2023-05-10 15:54:32 +0300

    fix(migration): исправил файл миграции

--- src/main/resources/db/migration/V3_addControlWork.sql
+++ src/main/resources/db/migration/V3_addControlWork.sql
@@ -5,10 +5,10 @@ INSERT INTO `control_option` (`id`, `control_option`,`control_work_id`) VALUES (
 INSERT INTO `control_option` (`id`, `control_option`,`control_work_id`) VALUES (6, 4, 5);
 INSERT INTO `control_option` (`id`, `control_option`,`control_work_id`) VALUES (7, 5, 5);
 INSERT INTO `control_option` (`id`, `control_option`,`control_work_id`) VALUES (8, 6, 5);
-INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (1, 3, 1);
-INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (2, 4, 1);
-INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (1, 5, 1);
-INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (2, 6, 1);
-INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (1, 7, 1);
-INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (2, 8, 1);
+INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (3, 3, 1);
+INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (4, 4, 1);
+INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (5, 5, 1);
+INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (6, 6, 1);
+INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (7, 7, 1);
+INSERT INTO `task_to_option` (`id`, `option_id`, `task_id`) VALUES (8, 8, 1);
 INSERT INTO `work_to_group` (`id`, `control_work_id`, `group_id`) VALUES (2, 5, 1);
